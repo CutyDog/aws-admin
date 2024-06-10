0c8eb5731141d253a68285690f545caba66ed130
CutyDog <silver.soul.818@gmail.com>
2024-06-10 21:20:11 +0900

    enable versioning

--- prd/s3.tf
+++ prd/s3.tf
@@ -1,3 +1,10 @@
-data "aws_s3_bucket" "tfstate" {
+resource "aws_s3_bucket" "tfstate" {
   bucket = "cutydog-organization-admin-tfstate"
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
